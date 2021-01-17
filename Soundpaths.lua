7e7c519a7e1f69f7e9a74d2d06b289e045b9c125
Duane Duffey <duaneduffey@gmail.com>
2021-01-17 15:33:47 +0100

    add addon files

---
+++ Soundpaths.lua
@@ -0,0 +1,29 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cFFFF0000CloudBurst|r", [[Interface\Addons\SharedMedia_Grooly\sound\CloudBurst.ogg]])
+LSM:Register("sound", "|cFFFF0000EarthenWall|r", [[Interface\Addons\SharedMedia_Grooly\sound\EarthenWall.ogg]])
+LSM:Register("sound", "|cFFFF0000HealingRain|r", [[Interface\Addons\SharedMedia_Grooly\sound\HealingRain.ogg]])
+LSM:Register("sound", "|cFFFF0000PrimordialWave|r", [[Interface\Addons\SharedMedia_Grooly\sound\PrimordialWave.ogg]])
+LSM:Register("sound", "|cFFFF0000Riptide|r", [[Interface\Addons\SharedMedia_Grooly\sound\Riptide.ogg]])
+LSM:Register("sound", "|cFFFF0000Wellspring|r", [[Interface\Addons\SharedMedia_Grooly\sound\Wellspring.ogg]])
+LSM:Register("sound", "|cFFFF0000EarthShield|r", [[Interface\Addons\SharedMedia_Grooly\sound\EarthShield.ogg]])
+LSM:Register("sound", "|cFFFF0000UnleashLife|r", [[Interface\Addons\SharedMedia_Grooly\sound\UnleashLife.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
