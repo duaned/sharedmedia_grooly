171fb84e690b7c5fa3ecdf164d96728f867837e6
Duane Duffey <duaneduffey@gmail.com>
2021-07-20 15:09:02 +0200

    Add pally cooldowns

--- Soundpaths.lua
+++ Soundpaths.lua
@@ -24,6 +24,9 @@ LSM:Register("sound", "|cFFFF0000Wellspring|r", [[Interface\Addons\SharedMedia_G
 LSM:Register("sound", "|cFFFF0000EarthShield|r", [[Interface\Addons\SharedMedia_Grooly\sound\EarthShield.ogg]])
 LSM:Register("sound", "|cFFFF0000UnleashLife|r", [[Interface\Addons\SharedMedia_Grooly\sound\UnleashLife.ogg]])
 LSM:Register("sound", "|cFFFF0000Innervate|r", [[Interface\Addons\SharedMedia_Grooly\sound\Innervate.ogg]])
+LSM:Register("sound", "|cFFFF0000Ashen|r", [[Interface\Addons\SharedMedia_Grooly\sound\Ashen.ogg]])
+LSM:Register("sound", "|cFFFF0000Wings|r", [[Interface\Addons\SharedMedia_Grooly\sound\Wings.ogg]])
+LSM:Register("sound", "|cFFFF0000RuleOfLaw|r", [[Interface\Addons\SharedMedia_Grooly\sound\RuleOfLaw.ogg]])
 
 -- -----
 --   STATUSBAR
